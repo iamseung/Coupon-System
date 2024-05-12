8f12181920da93666276b7c14380e93841b26ff6
iamseung <seesungseok@naver.com>
2024-05-12 22:11:41 +0900

    쿠폰 발급 테스트 초안
    - 쿠폰 유무에 따른 테스트 코드 작성

--- coupon-core/src/test/java/com/example/couponcore/service/CouponIssueServiceTest.java
+++ coupon-core/src/test/java/com/example/couponcore/service/CouponIssueServiceTest.java
@@ -1,10 +1,16 @@
 package com.example.couponcore.service;
 
 import com.example.couponcore.TestConfig;
+import com.example.couponcore.entity.CouponIssue;
+import com.example.couponcore.exception.CouponIssueException;
+import com.example.couponcore.exception.ErrorCode;
 import com.example.couponcore.repository.mysql.CouponIssueJpaRepository;
 import com.example.couponcore.repository.mysql.CouponIssueRepository;
 import com.example.couponcore.repository.mysql.CouponJpaRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,5 +32,36 @@ class CouponIssueServiceTest extends TestConfig {
     void clean() {
         couponJpaRepository.deleteAllInBatch();
         couponIssueJpaRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("쿠폰 발급 내역이 존재하면 예외를 반환한다")
+    void saveCouponIssue_1() {
+        // given
+        CouponIssue couponIssue = CouponIssue.builder()
+                .couponId(1L)
+                .userId(1L)
+                .build();
+
+        couponIssueJpaRepository.save(couponIssue);
+        // when & then
+        CouponIssueException exception = Assertions.assertThrows(CouponIssueException.class, () -> {
+            sut.saveCouponIssue(couponIssue.getCouponId(), couponIssue.getUserId());
+        });
+        Assertions.assertEquals(exception.getErrorCode(), ErrorCode.DUPLICATED_COUPON_ISSUE);
+    }
+
+    @Test
+    @DisplayName("쿠폰 발급 내역이 존재하지 않으면 쿠폰을 발급한다")
+    void saveCouponIssue_2() {
+        // given
+        long couponId = 1L;
+        long userId = 1L;
+
+        // when
+        CouponIssue result = sut.saveCouponIssue(couponId, userId);
+
+        // then
+        Assertions.assertTrue(couponIssueJpaRepository.findById(result.getId()).isPresent());
     }
 }
