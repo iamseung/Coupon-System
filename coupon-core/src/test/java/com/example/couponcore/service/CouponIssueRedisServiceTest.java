46ff572119c6499d3c8ce6ece5452b998b8ef8a6
iamseung <seesungseok@naver.com>
2024-05-27 23:36:39 +0900

    Redis 테스트 코드 작성
    - Redis 에서도 동일한 테스트 진행
    - 쿠폰 수량 검증

--- coupon-core/src/test/java/com/example/couponcore/service/CouponIssueRedisServiceTest.java
+++ coupon-core/src/test/java/com/example/couponcore/service/CouponIssueRedisServiceTest.java
@@ -1,9 +1,15 @@
 package com.example.couponcore.service;
 
 import com.example.couponcore.TestConfig;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Collection;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +23,35 @@ class CouponIssueRedisServiceTest extends TestConfig {
 
     @BeforeEach
     void clear() {
-        redisTemplate.keys("*");
+        Collection<String> redisKeys = redisTemplate.keys("*");
+        redisTemplate.delete(redisKeys);
+    }
+
+    @Test
+    @DisplayName("쿠폰 수량 검증 - 발급 가능 수량이 존재하면 true 반환")
+    void availableTotalIssueQuantity_1() {
+        // Given
+        int totalIssueQuantity = 10;
+        long userId = 1;
+
+        // When
+        boolean result = sut.availableTotalIssueQuantity(totalIssueQuantity, userId);
+
+        // Then
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("쿠폰 수량 검증 - 발급 가능 수량이 존재하면 true 반환")
+    void availableTotalIssueQuantity_2() {
+        // Given
+        int totalIssueQuantity = 10;
+        long userId = 1;
+
+        // When
+        boolean result = sut.availableTotalIssueQuantity(totalIssueQuantity, userId);
+
+        // Then
+        Assertions.assertTrue(result);
     }
 }
