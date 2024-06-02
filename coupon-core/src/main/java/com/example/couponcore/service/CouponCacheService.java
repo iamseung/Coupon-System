1180af9cfeb222f0a2f448ec07c3483e3b19b8cf
iamseung <seesungseok@naver.com>
2024-06-02 23:28:00 +0900

    쿠폰 캐시 서비스 구현
    - Redis 전용 쿠폰 엔티티
    - 구현 서비스
    - request dto

---
+++ coupon-core/src/main/java/com/example/couponcore/service/CouponCacheService.java
@@ -0,0 +1,19 @@
+package com.example.couponcore.service;
+
+import com.example.couponcore.entity.Coupon;
+import com.example.couponcore.repository.redis.dto.CouponRedisEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CouponCacheService {
+    private final CouponIssueService couponIssueService;
+
+    @Cacheable(cacheNames = "coupon")
+    public CouponRedisEntity getCouponCache(long couponId) {
+        Coupon coupon = couponIssueService.findCoupon(couponId);
+        return new CouponRedisEntity(coupon);
+    }
+}
