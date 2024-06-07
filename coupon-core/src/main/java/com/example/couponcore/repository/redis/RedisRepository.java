a8e6018e1f12ff7c6e99e8fcee869590b61a44a1
iamseung <seesungseok@naver.com>
2024-06-07 23:48:58 +0900

    Redis 스크립트 작성
    - 기존에 4단계의 로직을 Redis 스크립트로 처리

--- coupon-core/src/main/java/com/example/couponcore/repository/redis/RedisRepository.java
+++ coupon-core/src/main/java/com/example/couponcore/repository/redis/RedisRepository.java
@@ -2,6 +2,7 @@ package com.example.couponcore.repository.redis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
@@ -30,5 +31,22 @@ public class RedisRepository {
     // 리스트 생성
     public Long rPush(String key, String value) {
         return redisTemplate.opsForList().rightPush(key, value);
+    }
+
+    private RedisScript<String> issueRequestScript() {
+        String script = """
+                if redis.call('SISMEMBER', KEYS[1], ARGV[1]) == 1 then
+                    return '2'
+                end
+                                
+                if tonumber(ARGV[2]) > redis.call('SCARD', KEYS[1]) then
+                    redis.call('SADD', KEYS[1], ARGV[1])
+                    redis.call('RPUSH', KEYS[2], ARGV[3])
+                    return '1'
+                end
+                                
+                return '3'
+                """;
+        return RedisScript.of(script, String.class);
     }
 }
