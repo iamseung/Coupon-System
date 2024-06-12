a3be58f82508a504584f6216664820f0da43bc84
iamseung <seesungseok@naver.com>
2024-06-12 23:05:22 +0900

    쿠폰 소진 여부 확인
    - 필드로 검증
    - 메서들르 통한 예외처리

--- coupon-core/src/main/java/com/example/couponcore/repository/redis/dto/CouponRedisEntity.java
+++ coupon-core/src/main/java/com/example/couponcore/repository/redis/dto/CouponRedisEntity.java
@@ -3,7 +3,6 @@ package com.example.couponcore.repository.redis.dto;
 import com.example.couponcore.entity.Coupon;
 import com.example.couponcore.entity.CouponType;
 import com.example.couponcore.exception.CouponIssueException;
-import com.example.couponcore.exception.ErrorCode;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -12,11 +11,14 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 
 import static com.example.couponcore.exception.ErrorCode.INVALID_COUPON_ISSUE_DATE;
+import static com.example.couponcore.exception.ErrorCode.INVALID_COUPON_ISSUE_QUANTITY;
 
 public record CouponRedisEntity(
         Long id,
         CouponType couponType,
         Integer totalQuantity,
+        // 쿠폰 소진 여부
+        boolean availableIssueQuantity,
 
         @JsonSerialize(using = LocalDateTimeSerializer.class)
         @JsonDeserialize(using = LocalDateTimeDeserializer.class)
@@ -32,6 +34,7 @@ public record CouponRedisEntity(
                 coupon.getId(),
                 coupon.getCouponType(),
                 coupon.getTotalQuantity(),
+                coupon.availableIssueQuantity(),
                 coupon.getDateIssueStart(),
                 coupon.getDateIssueEnd()
         );
@@ -43,7 +46,10 @@ public record CouponRedisEntity(
     }
 
     public void checkIssuableCoupon() {
-        if(!availableIssueDate()) {
+        if (!availableIssueQuantity) {
+            throw new CouponIssueException(INVALID_COUPON_ISSUE_QUANTITY, "모든 발급 수량이 소진되었습니다. couponId : %s".formatted(id));
+        }
+        if (!availableIssueDate()) {
             throw new CouponIssueException(INVALID_COUPON_ISSUE_DATE, "발급 가능한 일자가 아닙니다. request : %s, issueStart: %s, issueEnd: %s".formatted(LocalDateTime.now(), dateIssueStart, dateIssueEnd));
         }
     }
